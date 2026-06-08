e30ca05ad592d5dd0870a6cc65620d621fc07f40
tech-tushar06 <tusharsharma80164@gmail.com>
2026-06-08 18:39:09 +0530

    Fix DB

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "/public")));    
 
+const dbUrl = process.env.ATLASDB_URL;
 
 const store = MongoStore.create ({
     mongoUrl: dbUrl,
